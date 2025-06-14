d717a054f35ed9caed8c425b8d9b46ce28adcb97
ruthiguta <cirurue@gmail.com>
2025-06-14 20:59:18 +0300

    serialization

--- server/models.py
+++ server/models.py
@@ -2,17 +2,23 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
 from sqlalchemy_serializer import SerializerMixin
 
+# imports, config
+
 metadata = MetaData()
 
 db = SQLAlchemy(metadata=metadata)
 
+# When a model class inherits from the SerializerMixin, it gains a range of methods for serializing and deserializing data.
+# These methods includeto_dict()`, which converts the model object into a dictionary.
+# SerializerMixin - is a superclass
 
-class Pet(db.Model):
-    __tablename__ = 'pets'
+
+class Pet(db.Model, SerializerMixin):
+    __tablename__ = "pets"
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
     species = db.Column(db.String)
 
     def __repr__(self):
-        return f'<Pet {self.id}, {self.name}, {self.species}>'
+        return f"<Pet {self.id}, {self.name}, {self.species}>"
